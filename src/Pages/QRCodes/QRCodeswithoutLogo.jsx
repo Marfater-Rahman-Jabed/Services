cf3286ad10b8334dc3861d5a0df849e3baede8e8
Marfater Rahman Jabed <marfaterrahman@gmail.com>
2023-12-01 22:24:26 +0600

    added sothing

--- src/Pages/QRCodes/QRCodeswithoutLogo.jsx
+++ src/Pages/QRCodes/QRCodeswithoutLogo.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import QRCode from 'qrcode';
 import DefaultQR from '../../assets/DevelQRCode.png'
+import useClipboard from 'react-use-clipboard';
 
 const QRCodeswithoutLogo = () => {
     const [url, setUrl] = useState('')
     const [show, setShow] = useState('')
     const [qrcode, setQrcode] = useState('')
+    const [isCopied, setCopied] = useClipboard(qrcode);
     const GenereteQR = () => {
         QRCode.toDataURL(url, (err, urls) => {
             if (err) return console.log(err)
@@ -18,7 +20,14 @@ const QRCodeswithoutLogo = () => {
 
     return (
         <div className=' bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-36 '>
-            {qrcode && <><h3 className='text-white text-center'>QR Code for <span className='text-black font-bold text-xl'>{show}</span></h3><br /></>}
+            {qrcode && <div className='flex justify-center gap-6 py-1'><h3 className='text-white text-center'>QR Code for <span className='text-black font-bold text-xl'>{show}</span></h3><div >
+                {/* <p>{qrcode?.slice(0, 23)}</p> */}
+                <button className="btn btn-success btn-xs" onClick={setCopied} >
+                    {isCopied ? 'Copied QR Code url' : 'Copy Text'}
+                </button>
+            </div><br /></div>}
+
+
             <div className='flex justify-center gap-4  '>
                 <div>
                     <textarea className="textarea textarea-secondary w-60 h-60" placeholder="Write Something..." value={url} onChange={(e) => setUrl(e.target.value)}></textarea><br />
@@ -31,6 +40,9 @@ const QRCodeswithoutLogo = () => {
                     <>
 
                         <img src={qrcode ? qrcode : DefaultQR} alt="" className='w-60 h-60' />
+
+
+
                         <div className='flex justify-center'>
                             <a className='btn btn-primary mt-4 px-20' href={qrcode} download={`${show.slice(0, 5)}QRCode.png`}>Download</a>
                         </div>
